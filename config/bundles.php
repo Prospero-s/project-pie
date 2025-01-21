97a33cfacd87e3315a1f0dbc4871cbbf9b314029
alo <a.on@thecodingmachine.com>
2025-01-21 16:56:06 +0100

    feat: allow to send Cross-Origin Resource Sharing headers

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
