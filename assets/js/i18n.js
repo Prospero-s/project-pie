4ac09e3cf9cbec1ac2813634e84cc0065258b30a
Sasorishi <on_alban@yahoo.fr>
2025-01-28 12:51:42 +0100

    styles(dashboard): PRSOP-14 - Change wording and add translation words fr/en

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -14,6 +14,8 @@ import frAllCompanies from './locales/fr/allCompanies.json';
 import enAllCompanies from './locales/en/allCompanies.json';
 import enGlobal from './locales/en/global.json';
 import frGlobal from './locales/fr/global.json';
+import enDashboard from './locales/en/dashboard.json';
+import frDashboard from './locales/fr/dashboard.json';
 
 i18n
   .use(initReactI18next)
@@ -26,6 +28,7 @@ i18n
         investments: frInvestments,
         allCompanies: frAllCompanies,
         global: frGlobal,
+        dashboard: frDashboard
       },
       en: {
         signin: enSignIn,
@@ -34,6 +37,7 @@ i18n
         investments: enInvestments,
         allCompanies: enAllCompanies,
         global: enGlobal,
+        dashboard: enDashboard
       },
     },
     lng: 'fr',
--- assets/js/pages/Dashboard.jsx
+++ assets/js/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
+import { useUser } from '@/context/userContext';
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 
 const Dashboard = ({ i18n }) => {
   const { t } = useTranslation('dashboard', { i18n });
+  const { user } = useUser();
   const lng = useParams().lng;
 
   useEffect(() => {
@@ -12,8 +14,8 @@ const Dashboard = ({ i18n }) => {
   
   return (
     <div>
-      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Welcome back</h2>
-      <p class="mt-4 text-gray-500">Track, manage and forecast your investments and manage your companies.</p>
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{t('message_start')}, {user.user_metadata.full_name ?? user.email}</h2>
+      <p class="mt-4 text-gray-500">{t('message_description')}</p>
     </div>
   );
 }
