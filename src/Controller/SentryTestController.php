663a69f217e656b006045a624bbb49d296884290
slorochi <kuro79779@gmail.com>
2025-07-03 21:56:22 +0200

    feat #PROS-134 ajout du sdk sentry dans le backend & frontend, modification vite.config.ts pour accéder à la variable d'env vite_sentry_dsn, configuration d'exemples de tests pour log/ catch errors en symfony

---
+++ src/Controller/SentryTestController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Sentry\State\HubInterface;
+use Sentry\Exception\ExceptionInterface;
+
+class SentryTestController extends AbstractController
+{
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
+    #[Route('/_sentry-test', name: 'sentry_test')]
+    public function testLog()
+    {
+        // the following code will test if monolog integration logs to sentry
+        $this->logger->error('My custom logged error.', ['some' => 'Context Data']);
+
+        // the following code will test if an uncaught exception logs to sentry
+        throw new \RuntimeException('Example exception.');
+    }
+
+    #[Route_('/_sentry-test-error', name: 'sentry_test_error')]
+    public function testError(HubInterface $sentryHub)
+    {
+        try {
+            throw new \RuntimeException('Example exception.');
+        } catch (\Throwable $e) {
+            $sentryHub->captureException($e);
+        }
+    }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv  } from 'vite';
 import symfonyPlugin from 'vite-plugin-symfony';
 import path from 'path';
 
 /* if you're using React */
 // import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+
   plugins: [
     /* react(), // if you're using React */
     // react(),
@@ -56,6 +59,8 @@ export default defineConfig({
     },
   },
   define: {
+          'import.meta.env.VITE_SENTRY_DSN': JSON.stringify(env.VITE_SENTRY_DSN),
+
     global: 'window',
     'process.env': process.env,
   },
@@ -66,4 +71,5 @@ export default defineConfig({
       },
     },
   },
+  }
 });
