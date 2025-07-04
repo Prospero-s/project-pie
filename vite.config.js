eea3b8ea16f512c6e5e825b7c3ecacdbd0b0b810
slorochi <kuro79779@gmail.com>
2025-07-04 22:39:42 +0200

    retrait code inutile vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 /* eslint-disable no-undef */
-import { defineConfig, loadEnv  } from 'vite';
+import { defineConfig } from 'vite';
 import symfonyPlugin from 'vite-plugin-symfony';
 import path from 'path';
 
 /* if you're using React */
 // import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  return {
-
+export default defineConfig({
   plugins: [
     /* react(), // if you're using React */
     // react(),
@@ -59,8 +56,6 @@ export default defineConfig(({ mode }) => {
     },
   },
   define: {
-          'import.meta.env.VITE_SENTRY_DSN': JSON.stringify(env.VITE_SENTRY_DSN),
-
     global: 'window',
     'process.env': process.env,
   },
@@ -71,5 +66,4 @@ export default defineConfig(({ mode }) => {
       },
     },
   },
-  }
 });
